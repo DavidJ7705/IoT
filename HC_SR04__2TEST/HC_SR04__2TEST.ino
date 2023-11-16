b664bfa9b99c48b01c0fe651b94366a82234d99d
G00419108@atu.ie <G00419108@atu.ie>
2023-11-16 13:31:40 +0000

    Added ultrasonic #2, didnt work with ultrasonic#1 on at the same time. works seperately in hc_sr04_2Test File

--- HC_SR04__2TEST/HC_SR04__2TEST.ino
+++ HC_SR04__2TEST/HC_SR04__2TEST.ino
@@ -1,8 +1,6 @@
 #include "Ultrasonic.h"
 
 // Define the pins for the ultrasonic sensor
-const int trigPin = 5;
-const int echoPin = 4;
 
 const int trigPin_2 = 17;
 const int echoPin_2 = 16;
@@ -13,17 +11,12 @@ const int LED = 0;
 #define BAUDRATE 115200
 
 // Variables to store the duration and distance
-long duration;
-int distance;
-
 long duration_2;
 int distance_2;
 
 void setup() {
  // Initialize serial communication
  Serial.begin(115200);
- pinMode(trigPin, OUTPUT);
- pinMode(echoPin, INPUT);
  pinMode(trigPin_2, OUTPUT);
  pinMode(echoPin_2, INPUT);
  pinMode(buzzer, OUTPUT);
@@ -31,11 +24,7 @@ void setup() {
 }
 void loop() {
  // Trigger the ultrasonic sensor to send a pulse
- digitalWrite(trigPin, LOW);
- delayMicroseconds(2);
- digitalWrite(trigPin, HIGH);
- delayMicroseconds(10);
- digitalWrite(trigPin, LOW);
+
 
  digitalWrite(trigPin_2, LOW);
  delayMicroseconds(2);
@@ -44,49 +33,18 @@ void loop() {
  digitalWrite(trigPin_2, LOW);
  
  // Measure the duration of the pulse on the echoPin
- duration = pulseIn(echoPin, HIGH);
  duration_2 = pulseIn(echoPin_2, HIGH);
  
  // Calculate the distance based on the speed of sound (343 meters/second)
- distance = duration * 0.0343 / 2;
  distance_2 = duration_2 * 0.0343 / 2;
  
  // Print the distance to the Serial Monitor
- Serial.print("Distance: ");
- Serial.print(distance);
- Serial.println(" cm");
 
  Serial.print("Distance 2: ");
  Serial.print(distance_2);
  Serial.println(" cm");
 
 
-//ultrasonic 1
- if(distance<100 && distance > 50){
-  digitalWrite(buzzer,HIGH);
-  digitalWrite(LED,HIGH);
-  delay(600);
-  digitalWrite(buzzer,LOW);
-  digitalWrite(LED,LOW);
-  delay(100);
- }
- 
- else if(distance<35 && distance > 20){
-  digitalWrite(buzzer,HIGH);
-  digitalWrite(LED,HIGH);
-  delay(30);
-  digitalWrite(buzzer,LOW);
-  digitalWrite(LED,LOW);
-  delay(50);
-  }
- else if(distance <20){
-  digitalWrite(buzzer,HIGH);
-  digitalWrite(LED,HIGH);
- } 
- else{
-  digitalWrite(buzzer,LOW);
-  digitalWrite(LED,LOW);
- }
 
  //ultrasonic 2
   if(distance_2<100 && distance_2 > 50){
